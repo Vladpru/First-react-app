78fc24255b94e18b7e9355c92053637b11b8c776
Vlad <vladllen96681@gmail.com>
2023-05-04 17:54:10 +0300

    first commit

--- src/Recipe.jsx
+++ src/Recipe.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Recipe = ({title, image, ingredients, calories}) => {
     return (
-        <div>
-          <h1>{title}</h1>
-          <ul>
+        <div className='recipe'>
+          <h1 className='recipe__title'>{title}</h1>
+          <ul className='recipe__list'>
             {ingredients.map((el, index) => (
-              <li key={index}>{el.text}</li>
+              <li className='recipe__item' key={index}>{el.text}</li>
             ))}
             
           </ul>
-          <p>{calories}</p>
+          <p className='recipe__cal'>Calories:{calories}</p>
           <img src={image} alt="img" />
         </div>
     );
